07e2871e9a5418fd67abc580fe5bb9929e43a5d9
仲一昕 <sjtuzhongyx@gmail.com>
2014-03-10 16:45:38 +0800

    thinkphp环境配置
    跟教程上的有点不一样

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+	define('APP_NAME','dangyuaninfo');
+	define('APP_PATH','./dangyuaninfo/');
+	define('RUNTIME_PATH','./dangyuaninfo/Temp');
+	define('APP_DEBUG',TRUE);
+	require './ThinkPHP/ThinkPHP.php';
